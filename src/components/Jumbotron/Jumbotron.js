62493cecd49e6427112ea1f8160cc3a04527c30b
Patrick Akpala <kemaakpala@gmail.com>
2018-03-23 18:49:17 +0000

    added appBar

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './Jumbotron.css'
 
 const jumbotron = () => (
     <div className="JumbotronBlock">
-        <div className="JumbotronHeader">
-            <h1>Patrick Here</h1>
-        </div>
-        <div ClassName="JumbotronContent">
+        <div className="JumbotronContent">
+            <div className="JumbotronHeader">
+                <h1>Patrick Here</h1>
+            </div>
             <p className="JumbotronDescription">Welcome to my website. I'm a fullstack developer with 5 years worth of experience. I love Keeping It Clean and Simple, I find this is always true with code as well as design. Hit me up, via any one of my social links and lets get started on building wonderful projects together, I'm looking forward to working with you.</p>
             <ul>
                 <li>facebook</li>
@@ -13,9 +14,9 @@ const jumbotron = () => (
                 <li>github</li>
                 <li>freeCodeCamp</li>
             </ul>
-            <div className="JumbotronImage">
-                <img src="http://via.placeholder.com/405x405" alt="imagename"/>
-            </div>
+        </div>
+        <div className="JumbotronImage">
+            <img src="http://via.placeholder.com/405x405" alt="imagename"/>
         </div>
     </div>
 )
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
+import './Menu.css'
+
 const menu = () => (
-    <nav>
-        Menu
-    </nav>
+
+    <div class="MenuBar">
+        <nav className="MenuNav"> 
+        <img class="Logo" src="http://via.placeholder.com/107x130" alt="Logo image" />
+            <ul className="MenuList">
+                <li>About</li>
+                <li>Skills</li>
+                <li>Contact</li>
+                <li>Medium</li>
+                <li>
+                    <img src="http://via.placeholder.com/50x50" alt="profile image"/>
+                    <ul className="MenuSubNav">
+                        <li>Logout</li>
+                        <li>Settings</li>
+                    </ul>
+                </li>
+                
+            </ul>
+        </nav>
+    </div>
 )
 
 export default menu
