e054a04aa7f8106ab509b14295b01f11607a3802
Patrick Akpala <kemaakpala@gmail.com>
2018-03-29 20:55:17 +0100

    made changes to jumboton component and admin containers

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import classes from './Jumbotron.css'
 import JumbotronImage from '../../assets/JumbotronImage.png'
+import Button from '../UI/Button/Button'
 
-const jumbotron = () => (
+const jumbotron = (props) => {
+    console.log(props);
+    return (
     <div className={classes.Jumbotron}>
         <div className={classes.JumbotronBlock}>
             <div className={classes.JumbotronContent}>
-                <h1 className={classes.Title1}>Patrick Here</h1>
-                <p className={classes.JumbotronDescription}>Welcome to my website. I'm a fullstack developer with 5 years worth of experience. I love Keeping It Clean and Simple, I find this is always true with code as well as design. Hit me up, via any one of my social links and lets get started on building wonderful projects together, I'm looking forward to working with you.</p>
+                <h1 className={classes.Title1}>{props.content.greeting}</h1>
+                <p className={classes.JumbotronDescription}>{props.content.description}</p>
                 <ul className={classes.SocialList}>
                     <li><i className="fab fa-linkedin  fa-lg"></i></li>
                     <li><i className="fab fa-twitter  fa-lg"></i></li>
@@ -15,9 +18,14 @@ const jumbotron = () => (
                     <li><i className="fab fa-free-code-camp  fa-lg"></i></li>
                 </ul>
             </div>
-            <img className={classes.JumbotronImage} src={JumbotronImage} alt="imagename"/>
+            <img className={classes.JumbotronImage} src={JumbotronImage} alt={props.content.AltText}/>
+            <div className={classes.JumbotronActions}>
+                <Button><i className="fas fa-edit"></i> Edit</Button>
+                <Button><i className="fas fa-save"></i> Save</Button>
+                <Button><i className="fas fa-trash-alt"></i> Delete</Button>
+            </div>
         </div>
     </div>
-)
+)}
 
 export default jumbotron
--- src/containers/AdminContainer/AdminContainer.js
+++ src/containers/AdminContainer/AdminContainer.js
@@ -5,12 +5,31 @@ import Skills from "../../components/Skills/Skills"
 import Contact from "../../components/Contact/Contact"
 
 class adminController extends Component {
-    render(){
+    state = {
+        jumbotron: {
+            content:{
+                greeting: "Patrick Here",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas velit purus, ornare viverra purus ac, convallis lacinia odio. Fusce interdum ac orci et placerat. Praesent scelerisque quis lorem sit amet facilisis. Mauris at dapibus dui. Sed sollicitudin velit arcu, non bibendum sem feugiat sit amet. Donec sodales nulla id tellus ornare, eu faucibus risus egestas",
+                image: {
+                    SrcUrl: '',
+                    AltText: 'imagename'
+                }
+            }
+        },
+        skills:{
+            content:{
+                
+            }
+        }
+    }
+
+    render () {
+        console.log(this.state.jumbotron.content);
         return (
             <Fragment>
-                <Jumbotron />
-                <Skills />
-                <Contact />
+                <Jumbotron content={this.state.jumbotron.content}/>
+                <Skills content={this.state.content}/>
+                <Contact content={this.state.content}/>
             </Fragment>
         )
     }
